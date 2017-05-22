6fc879ffaab4eaea34a662a31a8926653b5ce27f
vikdork <viktor.lindstrom@outlook.com>
2017-05-22 19:11:20 +0200

    test

--- group member remove date/add-usertogroup funtion.ps1
+++ group member remove date/add-usertogroup funtion.ps1
@@ -1,12 +1,12 @@
-﻿function add-usertogroup
-{
-param(
-[parameter(mandatory=$true)]
-$user,
-[parameter(mandatory=$true)]
-$group)
+﻿    function add-usertogroup
+    {
+    param(
+    [parameter(mandatory=$true)]
+    $user,
+    [parameter(mandatory=$true)]
+    $group)
 
-$aduser = get-aduser $user 
-$adgroup = get-adgroup $group
-Add-ADGroupMember -Identity $adgroup -Members $aduser
-}
+    $aduser = get-aduser $user 
+    $adgroup = get-adgroup $group
+    Add-ADGroupMember -Identity $adgroup -Members $aduser
+    }
