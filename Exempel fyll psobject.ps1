974d0f05794e75a0a9fbbe1bc7e652c2d28f902b
Lindström <viktor.lindstrom@huddinge.se>
2017-06-08 10:32:39 +0200

    Lite ändringar

--- Exempel fyll psobject.ps1
+++ Exempel fyll psobject.ps1
@@ -1,1 +1,14 @@
-$result = 
+$result = @()
+
+$test = Get-ChildItem
+
+$test.count
+    foreach ($item in $test)
+    {
+    $result += [pscustomobject][ordered] @{
+            'Filnamn' =  $item.name
+            'mode' = $item.mode 
+            'Tjo' = "Hej"
+        } 
+    }
+        $result | Export-Csv -Path c:\temp\test.csv -NoTypeInformation -Encoding UTF8 
