e0268be6b2667ec1a836817dbae97c51f82a5cbb
Lindström <viktor.lindstrom@huddinge.se>
2017-06-08 18:50:04 +0200

    Git it

--- Inventering servrar.ps1
+++ Inventering servrar.ps1
@@ -1,22 +1,39 @@
-$global:result = @()
-
 function Get-StaticRoutes {
+    [CmdletBinding()]
     param (
-        $computername
+        [parameter(ValueFromPipeline)]
+        [string[]]$computername
     )
-        
-        $scriptBlock = {Get-NetRoute | Where-Object {$_.NextHop -NE "::" -and $_.NextHop -ne "0.0.0.0"}}
-        $getRoutes = Invoke-Command -ComputerName $computername -Credential $cred -ScriptBlock $scriptBlock
-        $global:routes = [pscustomobject]@{
-                computername = $computername
-                NextHop = $getRoutes.NextHop
-                RouteMetric = $getRoutes.RouteMetric
-                DestinationPrefix = $getRoutes.DestinationPrefix
-                
-            }
+        $global:routes = @()
+        $getRoutes = Get-WmiObject -ClassName "win32_IP4RouteTable" -ComputerName $computername -Credential $cred | Where-Object {$_.NextHop -ne "0.0.0.0"}
+         
+                foreach ($route in $getRoutes) {
+                     $global:routes += [pscustomobject]@{
+                        computername = $Route.PSComputername
+                        NextHop = $Route.NextHop
+                        RouteMetric = $Route.Metric1
+                        Destination = $Route.Destination
+                        Mask = $Route.Mask
+                    }
+                    
+                }
+                $routes
+                Remove-Variable route
+               }
 
-}
+
+
+# $cred = Get-Credential    
+# Om man vill lägga in så dest och mask i CIDR.
+
+<#$Mask = $routes.Mask.Split(".")
+
+$val = 0
+$mask | foreach-object {$val = $val * 256 + [Convert]::ToInt64($_)}
+$ip + "/" + [Convert]::ToString($val,2).IndexOf('0')
+#>
+################################################################################################################################################
 
 
 
-#$cred = Get-Credential
+               
