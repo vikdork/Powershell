c6d220675ea9674a3871a49caa67c87faeaca617
Lindström <viktor.lindstrom@huddinge.se>
2017-06-29 15:03:32 +0200

    klart

---
+++ add_registry.ps1
@@ -0,0 +1,40 @@
+﻿
+#$adServers = Get-ADComputer -Filter {name -like "srv*" -or name -like "poc*" -or name -like "test*"}
+
+#$adServers | select Name -ExpandProperty name | Out-File -FilePath c:\temp\adcomp.txt -Encoding utf8
+
+$computername = Get-Content C:\temp\adcomp.txt
+
+
+    $dontExist = @()
+    $path = "HKLM:\SYSTEM\CurrentControlSet\services\SNMP\Parameters\PermittedManagers"
+    #$cred = Get-Credential
+    
+    <#
+
+        $script = { if (Test-Path $Using:path)
+            {
+        New-ItemProperty -Path $Using:path -Name 3 -Value "poc-wupapp01"
+                }
+                else 
+                    {
+                         $Global:dontExist+= $env:COMPUTERNAME
+                    }
+                
+                }
+         
+         #>
+            #$script = {Get-ItemProperty $Using:path}
+            
+            $script = {
+                       $testOm3Finns = Get-ItemProperty $using:path
+
+                       if ($testOm3Finns.PSObject.Properties['3']) {write-host "finns på $($env:COMPUTERNAME)"; $testOm3Finns | Select-Object "3"} 
+                } #>
+
+        Invoke-Command -ComputerName $computername -ScriptBlock $script -Credential $cred
+
+        $test = Invoke-Command -ComputerName $computername -ScriptBlock $script -Credential $cred
+
+
+
---
+++ iperf3.ps1
@@ -0,0 +1,9 @@
+﻿$cmd = "C:\Program Files\iperf3\iperf3.exe"
+$server = "huddp5132l2"
+$computer = "-c"
+
+$test = & $cmd $computer $server -J -f m | ConvertFrom-Json
+
+[decimal]$bits = ($test.intervals.streams.bits_per_second | Measure-Object -Average).Average
+
+[math]::Round($bits / 1MB)
